9ecf8b775ca8506bb2a7c62a32cd9b42af7b6e4c
Michael Hebestadt <mhebestadt@gmail.com>
2015-09-07 14:53:33 +0200

    Comment

---
+++ CateringKingCalculator/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace hebestadt.CateringKingCalculator.Converters
+{
+    public class DateTimeToStringConverter: IValueConverter
+    {
+        public object Convert(object value, Type type, object parameter, string language)
+        {
+            string result = "";
+
+            try
+            {
+                if (value is DateTimeOffset)
+                {
+                    DateTimeOffset theDate = (DateTimeOffset)value;
+                    result = string.Format("{0:ddd dd.MM.yyyy}", theDate);
+                }
+                else
+                {
+                    DateTimeOffset theDate = DateTimeOffset.Now;
+                    result = string.Format("{0:ddd dd.MM.yyyy}", theDate);
+                }
+            }
+            catch (FormatException e)
+            {
+                string erroMessage = e.Message;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type type, object parameter, string language)
+        {
+            object result = null;
+            try
+            {
+                result =  System.Convert.ToDateTime(value);
+            }
+            catch(FormatException e)
+            {
+                string erroMessage = e.Message;
+            }
+
+            return result;
+        }
+    }
+
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type type, object parameter, string language)
+        {
+            string result = "";
+
+            result = value.ToString();
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type type, object parameter, string language)
+        {
+            return Int32.Parse((string)value);
+
+        }
+    }
+}
