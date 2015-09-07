39fa210578edfbca031b807dd291461a8cc72414
Michael Hebestadt <mhebestadt@gmail.com>
2015-09-07 14:55:52 +0200

    The first one...

---
+++ CateringKingCalculator/Comparerers/CategoryCompare.cs
@@ -0,0 +1,25 @@
+﻿using hebestadt.CateringKingCalculator.Models;
+using hebestadt.CateringKingCalculator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hebestadt.CateringKingCalculator.Comparerers
+{
+    public class CategoryCompare : Comparer<MealItemViewModel>
+    {
+        public override int Compare(MealItemViewModel x, MealItemViewModel y)
+        {
+            if (x.CategoryId.CompareTo(y.CategoryId) != 0)
+            {
+                return x.CategoryId.CompareTo(y.CategoryId);
+            }
+            else
+                return 0;
+        }
+
+    }
+}
