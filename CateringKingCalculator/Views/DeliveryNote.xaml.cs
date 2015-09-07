d07f3091c9e09234e2669f971c905d2460ceccf7
Michael Hebestadt <mhebestadt@gmail.com>
2015-09-07 15:01:11 +0200

    Initial check-in

---
+++ CateringKingCalculator/Views/DeliveryNote.xaml.cs
@@ -0,0 +1,209 @@
+﻿using hebestadt.CateringKingCalculator.ViewModels;
+using System;
+using System.Collections.Generic;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CateringKingCalculator.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DeliveryNote : Page
+    {
+        MealViewModel meal = null;
+
+        public DeliveryNote()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            if (e.Parameter != null)
+            {
+                meal = (MealViewModel)e.Parameter;
+
+                editor.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, meal.GetTextRepresentation(meal));
+                
+                /*editor.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, "0123456789\n0123456789");
+                editor.Document.Selection.StartPosition = 9;
+                editor.Document.Selection.EndPosition = 12;
+                ITextSelection sel = editor.Document.Selection;
+
+                if (sel != null)
+                {
+                    ITextCharacterFormat charFormatting = sel.CharacterFormat;
+                    charFormatting.Bold = FormatEffect.Toggle;
+                    //charFormatting.Size = 20;
+                    //charFormatting.BackgroundColor = Colors.Red;
+                    charFormatting.ForegroundColor = Colors.Red;
+                    sel.CharacterFormat = charFormatting;
+                }*/
+            }
+        }
+    
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a text file.
+            Windows.Storage.Pickers.FileOpenPicker open =
+                new Windows.Storage.Pickers.FileOpenPicker();
+            open.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            open.FileTypeFilter.Add(".rtf");
+
+            Windows.Storage.StorageFile file = await open.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+                // Load the file into the Document property of the RichEditBox.
+                editor.Document.LoadFromStream(Windows.UI.Text.TextSetOptions.FormatRtf, randAccStream);
+            }
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string theText;
+            editor.Document.GetText(Windows.UI.Text.TextGetOptions.FormatRtf, out theText);
+
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
+
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until we 
+                // finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+                // write to file
+                Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+
+                editor.Document.SaveToStream(Windows.UI.Text.TextGetOptions.FormatRtf, randAccStream);
+
+                // Let Windows know that we're finished changing the file so the 
+                // other app can update the remote version of the file.
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                if (status != FileUpdateStatus.Complete)
+                {
+                    Windows.UI.Popups.MessageDialog errorBox =
+                        new Windows.UI.Popups.MessageDialog("File " + file.Name + " couldn't be saved.");
+                    await errorBox.ShowAsync();
+                }
+            }
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            ITextSelection selectedText = editor.Document.Selection;
+            if (selectedText != null)
+            {
+                ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
+                charFormatting.Bold = FormatEffect.Toggle;
+                selectedText.CharacterFormat = charFormatting;
+            }
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            ITextSelection selectedText = editor.Document.Selection;
+            if (selectedText != null)
+            {
+                ITextCharacterFormat charFormatting = selectedText.CharacterFormat;
+                charFormatting.Italic = FormatEffect.Toggle;
+                selectedText.CharacterFormat = charFormatting;
+            }
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedColor = (Button)sender;
+
+            ITextCharacterFormat charFormatting = editor.Document.Selection.CharacterFormat;
+            switch (clickedColor.Name)
+            {
+                case "black":
+                    {
+                        charFormatting.ForegroundColor = Colors.Black;
+                        break;
+                    }
+
+                case "gray":
+                    {
+                        charFormatting.ForegroundColor = Colors.Gray;
+                        break;
+                    }
+
+                case "greenyellow":
+                    {
+                        charFormatting.ForegroundColor = Colors.GreenYellow;
+                        break;
+                    }
+
+                case "green":
+                    {
+                        charFormatting.ForegroundColor = Colors.Green;
+                        break;
+                    }
+
+                case "blue":
+                    {
+                        charFormatting.ForegroundColor = Colors.Blue;
+                        break;
+                    }
+
+                case "red":
+                    {
+                        charFormatting.ForegroundColor = Colors.Red;
+                        break;
+                    }
+
+                default:
+                    {
+                        charFormatting.ForegroundColor = Colors.Black;
+                        break;
+                    }
+            }
+            editor.Document.Selection.CharacterFormat = charFormatting;
+
+            editor.Focus(Windows.UI.Xaml.FocusState.Keyboard);
+            fontColorButton.Flyout.Hide();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width <= 768)
+            {
+                editor.Width = e.NewSize.Width - 20;
+            }
+            else
+            {
+                editor.Width = e.NewSize.Width - 100;
+            }
+        }
+
+        private void NavigateBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MealItems), meal);
+        }
+    }
+}
