15c0c8db45c809e00f1e2e092a7e59e99ce83af4
Michael Hebestadt <mhebestadt@gmail.com>
2015-09-25 19:17:58 +0200

    initial check-in

---
+++ CateringKingCalculator/Models/MealSuggestion.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace hebestadt.CateringKingCalculator.Models
+{
+    class MealSuggestion
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public byte[] MealItemIDsWithWeight { get; set; }
+    }
+}
