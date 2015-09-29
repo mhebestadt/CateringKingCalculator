cb29656dcddb4b8d57a44213bf8ba96de3f79c67
Michael Hebestadt <mhebestadt@gmail.com>
2015-09-29 23:13:40 +0200

    initial check-in

---
+++ CateringKingCalculatorTests/Converters/ConverterTestBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace CateringKingCalculatorTests.Converters
+{
+    public abstract class ConverterTestBase
+    {
+        public void ConvertWithNullParameter_ShouldThrowArgumentNullException(IValueConverter converter)
+        {
+            int weekOfYear = 0;
+
+            try
+            {
+                weekOfYear = (int)converter.Convert(null, null, null, "");
+            }
+            catch (ArgumentNullException exception)
+            {
+                StringAssert.Contains(exception.Message, "");
+            }
+        }
+
+        public void ConvertBackWithNullParameter_ShouldThrowArgumentNullException(IValueConverter converter)
+        {
+            DateTime[] result = null;
+
+            try
+            {
+                result = (DateTime[])converter.ConvertBack(null, null, null, "");
+            }
+            catch (ArgumentNullException exception)
+            {
+                StringAssert.Contains(exception.Message, "");
+            }
+        }
+    }
+}
