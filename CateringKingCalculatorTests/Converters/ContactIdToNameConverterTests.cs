fe73a8340e466ce684e0d57b771b83a7f1eae588
Michael Hebestadt <mhebestadt@gmail.com>
2015-09-29 23:13:56 +0200

    initial check-in

---
+++ CateringKingCalculatorTests/Converters/ContactIdToNameConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using hebestadt.CateringKingCalculator.Converters;
+using CateringKingCalculatorTests.Converters;
+
+namespace hebestadt.CateringKingCalculatorTests.Converters
+{
+    [TestClass]
+    public class ContactIdToNameConverterTests : ConverterTestBase
+    {
+        private static ContactIdToNameConverter converter = new ContactIdToNameConverter();
+
+        [TestMethod]
+        public void ConvertWithNullParameter_ShouldThrowArgumentNullException()
+        {
+            this.ConvertWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+
+        [TestMethod]
+        public void ConvertBackWithNullParameter_ShouldThrowArgumentNullException()
+        {
+            this.ConvertBackWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+    }
+}
