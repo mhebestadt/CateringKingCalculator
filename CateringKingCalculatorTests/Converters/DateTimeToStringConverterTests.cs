23c6897e05f1fb99c7f31d7b03cc4ef194c68d9e
Michael Hebestadt <mhebestadt@gmail.com>
2015-11-05 17:27:20 +0100

    Initial check-in

---
+++ CateringKingCalculatorTests/Converters/DateTimeToStringConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using CateringKingCalculatorTests.Converters;
+using hebestadt.CateringKingCalculator.Converters;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace hebestadt.CateringKingCalculatorTests.Converters
+{
+    class DateTimeToStringConverterTests : ConverterTestBase
+    {
+        DateTimeToStringConverter converter = new DateTimeToStringConverter();
+
+        [TestMethod]
+        public void ConvertWithNullParameter_ShouldThrowArgumentNullException()
+        {
+            this.ConvertWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+
+        [TestMethod]
+        public void ConvertBackWithNullParameter_ShouldThrowArgumentNullException()
+        {
+            this.ConvertBackWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+    }
+}
