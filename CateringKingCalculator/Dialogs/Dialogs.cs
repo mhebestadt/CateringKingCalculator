2cc7c3db43d3d6b10c5b406c77ab5eba9b79105a
Michael Hebestadt <mhebestadt@gmail.com>
2015-11-10 11:31:18 +0100

    Moved file back to original dir

---
+++ CateringKingCalculator/Dialogs/Dialogs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Popups;
+
+
+namespace hebestadt.CateringKingCalculator.Dialogs
+{
+    static class Dialogs
+    {
+        public static async void ShowYesNoDialog(string message, UICommandInvokedHandler invokeHandler)
+        {
+            var messageDialog = new MessageDialog(message);
+
+            messageDialog.Commands.Add(new UICommand(
+                "Ja",
+                new UICommandInvokedHandler(invokeHandler)));
+
+            messageDialog.Commands.Add(new UICommand(
+                "Nein",
+                new UICommandInvokedHandler(invokeHandler)));
+
+            messageDialog.DefaultCommandIndex = 0;
+            messageDialog.CancelCommandIndex = 1;
+            await messageDialog.ShowAsync();
+        }
+    }
+}
