1218bc37f308c2aa17d396fc2f2b4c3d9d722504
Michael Hebestadt <mhebestadt@gmail.com>
2015-11-10 14:14:49 +0100

    Initial check-in

---
+++ CateringKingCalculatorTests/Converters/GenericDictionaryToByteArrayTests.cs
@@ -0,0 +1,23 @@
+﻿using CateringKingCalculator.Converters;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace CateringKingCalculatorTests.Converters
+{
+    [TestClass]
+    class GenericDictionaryToByteArrayTests : ConverterTestBase
+    {
+        private static GenericDictionaryToByteArray converter = new GenericDictionaryToByteArray();
+
+        [TestMethod]
+        public void ConvertWithNullParameter_ShouldThrowArgumentNullException()
+        {
+            this.ConvertWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+
+        [TestMethod]
+        public void ConvertBackWithNullParameter_ShouldThrowArgumentNullException()
+        {
+            this.ConvertBackWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+    }
+}
