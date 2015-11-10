8968c6a7354738311092213701e53124d19f7f9d
Michael Hebestadt <mhebestadt@gmail.com>
2015-11-10 15:46:26 +0100

    Initial check-in

--- CateringKingCalculatorTests/Converters/GenericDictionaryToByteArrayTests.cs
+++ CateringKingCalculatorTests/Converters/GenericDictionaryToByteArrayTests.cs
@@ -1,10 +1,11 @@
 ﻿using CateringKingCalculator.Converters;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Collections.Generic;
 
 namespace CateringKingCalculatorTests.Converters
 {
     [TestClass]
-    class GenericDictionaryToByteArrayTests : ConverterTestBase
+    public class GenericDictionaryToByteArrayTests : ConverterTestBase
     {
         private static GenericDictionaryToByteArray converter = new GenericDictionaryToByteArray();
 
@@ -18,6 +19,18 @@ namespace CateringKingCalculatorTests.Converters
         public void ConvertBackWithNullParameter_ShouldThrowArgumentNullException()
         {
             this.ConvertBackWithNullParameter_ShouldThrowArgumentNullException(converter);
+        }
+
+        [TestMethod]
+        public void ConvertAndBackTestValues()
+        {
+            byte[] theArray = (byte[])converter.Convert(new Dictionary<int, int>() { { 1, 15 }, { 2, 20 }, { 3, 30 }, { 4, 40 } });
+            Dictionary<int, int> theDict = new Dictionary<int, int>();
+            theDict = (Dictionary<int, int>)converter.ConvertBack(theArray);
+            Assert.IsTrue(theDict[1] == 15);
+            Assert.IsTrue(theDict[2] == 20);
+            Assert.IsTrue(theDict[3] == 30);
+            Assert.IsTrue(theDict[4] == 40);
         }
     }
 }
